9e6e3c9e051f6942b0ee4789f926f5bbcf0ffa08
Gail Eddy <gaileddy68@gmail.com>
2021-10-19 10:31:42 -0500

    fixed controller and route

--- app/controllers/phrases_controller.rb
+++ app/controllers/phrases_controller.rb
@@ -1,3 +1,7 @@
 class PhrasesController < ApplicationController
-  get "/phrase_params" => "phrases#phrase_params"
+  def phrase_params
+    input_phrase = params["query_phrase"]
+    output_phrase = input_phrase.upcase
+    render json: { message: output_phrase }
+  end
 end
